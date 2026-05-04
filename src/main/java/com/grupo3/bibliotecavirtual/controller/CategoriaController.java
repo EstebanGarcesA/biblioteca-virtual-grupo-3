d7710d861617eb20046cd3276e0c671acd188576
lorenamejiag <loregalvan0930@gmail.com>
2026-05-03 23:14:05 -0500

    refactor: implementar DTOs en categoria, agregar request/response y mapping en service

--- src/main/java/com/grupo3/bibliotecavirtual/controller/CategoriaController.java
+++ src/main/java/com/grupo3/bibliotecavirtual/controller/CategoriaController.java
@@ -1,8 +1,8 @@
 package com.grupo3.bibliotecavirtual.controller;
 
-import com.grupo3.bibliotecavirtual.model.dto.CategoriaDTO;
-import com.grupo3.bibliotecavirtual.model.entity.Categoria;
-import com.grupo3.bibliotecavirtual.service.CategoriaService;
+import com.grupo3.bibliotecavirtual.model.dto.CategoriaRequestDTO;
+import com.grupo3.bibliotecavirtual.model.dto.CategoriaResponseDTO;
+import com.grupo3.bibliotecavirtual.service.impl.CategoriaServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,50 +11,19 @@ import java.util.List;
 @RequestMapping("/categorias")
 public class CategoriaController {
 
-    private final CategoriaService service;
+    private final CategoriaServiceImpl service;
 
-    public CategoriaController(CategoriaService service) {
+    public CategoriaController(CategoriaServiceImpl service) {
         this.service = service;
     }
 
     @GetMapping
-public List<CategoriaDTO> listar() {
-
-    return service.listar().stream().map(cat -> {
-        CategoriaDTO dto = new CategoriaDTO();
-        dto.setId(cat.getId());
-        dto.setNombre(cat.getNombre());
-        dto.setDescripcion(cat.getDescripcion());
-        return dto;
-    }).toList();
-}
-
-    @PostMapping
-public Categoria guardar(@RequestBody CategoriaDTO dto) {
-
-    Categoria categoria = new Categoria();
-    categoria.setNombre(dto.getNombre());
-    categoria.setDescripcion(dto.getDescripcion());
-
-    return service.guardar(categoria);
-}
-
-    @PutMapping("/{id}")
-public Categoria actualizar(@PathVariable Long id, @RequestBody CategoriaDTO dto) {
-
-    Categoria categoria = new Categoria();
-    categoria.setNombre(dto.getNombre());
-    categoria.setDescripcion(dto.getDescripcion());
-
-    return service.actualizar(id, categoria);
-}
-    @DeleteMapping("/{id}")
-    public void eliminar(@PathVariable Long id) {
-        service.eliminar(id);
+    public List<CategoriaResponseDTO> listar() {
+        return service.listarDTO();
     }
 
-    @GetMapping("/{id}")
-    public Categoria buscar(@PathVariable Long id) {
-        return service.buscarPorId(id);
+    @PostMapping
+    public CategoriaResponseDTO guardar(@RequestBody CategoriaRequestDTO dto) {
+        return service.guardarDTO(dto);
     }
 }
