7f8debb998ef6ddf7db1373535bf7aaf5ff67c14
CarolinaBolivar5 <cbolivar12@cesde.net>
2026-05-06 19:50:00 -0500

    (docs): Configuracion de la documentacion con OpenApi de Libro y Estado

--- src/main/java/com/grupo3/bibliotecavirtual/controller/EstadoController.java
+++ src/main/java/com/grupo3/bibliotecavirtual/controller/EstadoController.java
@@ -2,6 +2,8 @@ package com.grupo3.bibliotecavirtual.controller;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,33 +18,39 @@ import com.grupo3.bibliotecavirtual.model.entity.Estado;
 import com.grupo3.bibliotecavirtual.service.EstadoService;
 
 @RestController
+@Tag(name = "Estado", description = "Gestión de estados")
 @RequestMapping("/api/estados")
 public class EstadoController {
 
     @Autowired
     private EstadoService estadoService;
 
+    @Operation(summary = "Obtener todos los estados creados")
     @GetMapping
     public List<Estado> listar() {
         return estadoService.listarEstados();
     }
 
+    @Operation(summary = "Obtener un estado por ID")
     @GetMapping("/{id}")
     public Estado obtenerPorId(@PathVariable Long id) {
         return estadoService.obtenerEstadoPorId(id);
     }
 
+    @Operation(summary = "Crear un estado")
     @PostMapping
     public Estado guardar(@RequestBody Estado estado) {
         return estadoService.guardarEstado(estado);
     }
 
+    @Operation(summary = "Actualizar un estado por id")
     @PutMapping("/{id}")
     public Estado actualizar(@PathVariable Long id, @RequestBody Estado estado) {
         estado.setId(id);
         return estadoService.guardarEstado(estado);
     }
 
+    @Operation(summary = "Eliminar un estado por id")
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable Long id) {
         estadoService.eliminarEstado(id);
