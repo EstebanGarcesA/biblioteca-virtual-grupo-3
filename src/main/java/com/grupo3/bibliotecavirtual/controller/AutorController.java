34b8a4a3ba99527863ecdcde23a2214371a576f5
EstebanGarcesA <esteg254@gmail.com>
2026-05-06 19:29:02 -0500

    (docs): configuración de la documentación con OpenApi General y de autor

--- src/main/java/com/grupo3/bibliotecavirtual/controller/AutorController.java
+++ src/main/java/com/grupo3/bibliotecavirtual/controller/AutorController.java
@@ -2,11 +2,14 @@ package com.grupo3.bibliotecavirtual.controller;
 
 import com.grupo3.bibliotecavirtual.model.entity.Autor;
 import com.grupo3.bibliotecavirtual.service.AutorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Tag(name = "Autores", description = "Gestión de autores")
 @RequestMapping("/autores")
 public class AutorController {
 
@@ -16,21 +19,25 @@ public class AutorController {
         this.autorService = autorService;
     }
 
+    @Operation(summary = "Crear autores")
     @PostMapping
     public Autor guardar(@RequestBody Autor autor) {
         return autorService.guardarAutor(autor);
     }
 
+    @Operation(summary = "Obtener todos los autores creados")
     @GetMapping
     public List<Autor> listar() {
         return autorService.listarAutores();
     }
 
+    @Operation(summary = "Obtener un autor por ID")
     @GetMapping("/{id}")
     public Autor buscar(@PathVariable Long id) {
         return autorService.buscarPorId(id);
     }
 
+    @Operation(summary = "Eliminar un autor por id")
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable Long id) {
         autorService.eliminarAutor(id);
