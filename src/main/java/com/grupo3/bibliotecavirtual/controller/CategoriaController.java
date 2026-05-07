24198c991221d7cfd37e0c06949aa2319eda3c5c
lorenamejiag <loregalvan0930@gmail.com>
2026-05-06 19:50:11 -0500

    feact: ajuste de documentacion en la entidad categoria

--- src/main/java/com/grupo3/bibliotecavirtual/controller/CategoriaController.java
+++ src/main/java/com/grupo3/bibliotecavirtual/controller/CategoriaController.java
@@ -1,11 +1,18 @@
 package com.grupo3.bibliotecavirtual.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.grupo3.bibliotecavirtual.model.dto.CategoriaRequestDTO;
 import com.grupo3.bibliotecavirtual.model.dto.CategoriaResponseDTO;
 import com.grupo3.bibliotecavirtual.service.impl.CategoriaServiceImpl;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/categorias")
@@ -16,12 +23,12 @@ public class CategoriaController {
     public CategoriaController(CategoriaServiceImpl service) {
         this.service = service;
     }
-
+    @Operation(summary = "Obtener todas las categorias creadas")
     @GetMapping
     public List<CategoriaResponseDTO> listar() {
         return service.listarDTO();
     }
-
+    @Operation(summary = "Crear categorias")
     @PostMapping
     public CategoriaResponseDTO guardar(@RequestBody CategoriaRequestDTO dto) {
         return service.guardarDTO(dto);
