3933a976a61f5d9e4080f2019740fa5afd4bf3d3
EstebanGarcesA <esteg254@gmail.com>
2026-05-06 19:45:57 -0500

    (docs): Configuración de documentación para rol

--- src/main/java/com/grupo3/bibliotecavirtual/controller/RolController.java
+++ src/main/java/com/grupo3/bibliotecavirtual/controller/RolController.java
@@ -3,6 +3,8 @@ package com.grupo3.bibliotecavirtual.controller;
 import com.grupo3.bibliotecavirtual.model.dto.RolDTO;
 import com.grupo3.bibliotecavirtual.model.entity.Rol;
 import com.grupo3.bibliotecavirtual.service.RolService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Tag(name = "Roles", description = "Gestión de roles")
 @RequestMapping("/roles")
 public class RolController {
 
@@ -19,16 +22,19 @@ public class RolController {
         this.service = service;
     }
 
+    @Operation(summary = "Listar Roles")
     @GetMapping
     public ResponseEntity<List<Rol>> listar() {
         return ResponseEntity.ok(service.listar());
     }
 
+    @Operation(summary = "Obtener un rol por ID")
     @GetMapping("/{id}")
     public ResponseEntity<Rol> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(service.buscarPorId(id));
     }
 
+    @Operation(summary = "Crear rol")
     @PostMapping
     public ResponseEntity<Rol> guardar(@RequestBody RolDTO dto) {
         Rol rol = new Rol();
@@ -36,6 +42,7 @@ public class RolController {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(rol));
     }
 
+    @Operation(summary = "Actualizar rol por ID")
     @PutMapping("/{id}")
     public ResponseEntity<Rol> actualizar(@PathVariable Long id, @RequestBody RolDTO dto) {
         Rol rol = new Rol();
@@ -43,6 +50,7 @@ public class RolController {
         return ResponseEntity.ok(service.actualizar(id, rol));
     }
 
+    @Operation(summary = "Eliminar un rol por id")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminar(@PathVariable Long id) {
         service.eliminar(id);
