49795739da500fb71ca5c746273a33e42545739d
EstebanGarcesA <esteg254@gmail.com>
2026-05-26 16:38:26 -0500

    (chore): refactorizar el spring security para permitir acceso a todos los endpoints

--- src/main/java/com/grupo3/bibliotecavirtual/config/SecurityConfig.java
+++ src/main/java/com/grupo3/bibliotecavirtual/config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.grupo3.bibliotecavirtual.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,67 +10,21 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/v3/api-docs/**",
-                                "/swagger-ui/**",
-                                "/swagger-ui.html",
-                                "/scalar/**"
-                        ).permitAll()
-                        .requestMatchers(HttpMethod.POST,
-                                "/usuarios",
-                                "/api/libros",
-                                "/api/libros/**",
-                                "/categorias",
-                                "/categorias/**",
-                                "/autores",
-                                "/autores/**",
-                                "/api/estados",
-                                "/api/estados/**",
-                                "/roles",
-                                "/roles/**",
-                                "/perfiles/**",
-                                "/prestamos/**"
-                        ).permitAll()
-                        .requestMatchers(HttpMethod.GET, "/usuarios").permitAll()
-
-                        .requestMatchers(HttpMethod.GET,
-                                "/api/libros",
-                                "/api/libros/**",
-                                "/categorias",
-                                "/categorias/**",
-                                "/autores",
-                                "/autores/**",
-                                "/api/estados",
-                                "/api/estados/**",
-                                "/roles",
-                                "/roles/**",
-                                "/perfiles/**",
-                                "/prestamos/**"
-                        ).permitAll()
-                        .requestMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/api/libros", "/api/libros/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/api/libros", "/api/libros/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/categorias", "/categorias/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/autores", "/autores/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/api/estados", "/api/estados/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/api/estados", "/api/estados/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/usuarios", "/usuarios/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/usuarios", "/usuarios/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/roles", "/roles/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/roles", "/roles/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/perfiles/**").hasRole("USUARIO")
-                        .requestMatchers(HttpMethod.POST,"/perfiles/**").hasRole("USUARIO")
-                        .anyRequest().authenticated()
-                )
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
-
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                    "/v3/api-docs/**",
+                    "/swagger-ui/**",
+                    "/swagger-ui.html",
+                    "/scalar/**"
+                ).authenticated()
+                .anyRequest().permitAll()
+            )
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
